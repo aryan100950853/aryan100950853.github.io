025b26bd3348a7a2c0dc4feb1331397b221fd22a
aryan100950853 <aryankumar.panchal@dcmail.ca>
2024-07-31 14:42:19 -0400

    Second Commit
    Add the File type , date and the description .

--- Assignment 4/Part_1.js
+++ Assignment 4/Part_1.js
@@ -1,4 +1,9 @@
-
+/**
+ * Name : Aryankuamar panchal 
+ * Date : 2024/07/31
+ * File type : .Js 
+ * Description : Assignment 4 JavaScript File for the Silly story generator.
+ */
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
