cfb688541b877cd6d9c5d64b6e78c7ded32ed082
aryan100950853 <aryankumar.panchal@dcmail.ca>
2024-07-31 15:43:35 -0400

    final commit

--- Assignment 4/Part_1.js
+++ Assignment 4/Part_1.js
@@ -21,7 +21,16 @@ const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewal
 randomize.addEventListener('click', result);
 
 function result() {
+    let newStory = storyText;
 
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+  
+    newStory = newStory.replaceAll(':insertx:',xItem);
+    newStory = newStory.replaceAll(':inserty:',yItem);
+    newStory = newStory.replaceAll(':insertz:',zItem);
+  
 
   if (customName.value !== '') {
     const name = customName.value;
@@ -29,8 +38,10 @@ function result() {
   }
 
   if (document.getElementById("uk").checked) {
-    const weight = Math.round(300);
-    const temperature =  Math.round(94);
+    const weight = `${Math.round(300)} stone`;
+    const temperature =  `${Math.round((94-32) * 5 / 9)} centigrade`;
+    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    newStory = newStory.replaceAll('300 pounds', weight);
   }
 
   story.textContent = newStory;
