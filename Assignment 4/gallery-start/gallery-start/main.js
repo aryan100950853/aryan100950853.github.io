c2fd343a202060678dcf084c389c429d2995a766
aryan100950853 <aryankumar.panchal@dcmail.ca>
2024-07-31 16:59:26 -0400

    2 commit
    Add the header comment in all files with the name , date, file type, and short description.

--- Assignment 4/gallery-start/gallery-start/main.js
+++ Assignment 4/gallery-start/gallery-start/main.js
@@ -1,3 +1,20 @@
+/*
+
+         Name: Aryankumar Panchal
+
+         File: mail.js
+
+         Date: 2024/07/31
+
+         This JavaScript file creates an image gallery when the thumbnail images are clicked, 
+         the images displayed in a larger box change. It also entails operations for the button 
+         that switches the opacity between the darker and lighter version of the overlay on 
+         the shown image for an aesthetic feature. The thumbnails are created using PHP where 
+         there is a predefined list of images and their corresponding alt tags to display as 
+         thumbnails and the process of making the image darker or lighter is under the button 
+         and disabled/enabled by adding class and style to the button.
+
+*/
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
 
