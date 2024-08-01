13f16f5870eff951040d6b06ca19b296231b0aaf
aryan100950853 <aryankumar.panchal@dcmail.ca>
2024-08-01 15:33:41 -0400

    4 commit
    Added `draw()` method to `Ball` class for rendering the ball on the canvas with its specified color and size.Implemented `update()` method to adjust ball position and handle edge bouncing. Added `collisionDetect()` method to check for collisions with other balls and change colors upon collision.

--- Assignment 4/p_3/main.js
+++ Assignment 4/p_3/main.js
@@ -41,4 +41,38 @@ class Ball {
     this.velY = velY;
     this.color = color;
     this.size = size;
-  }}
+  }
+  
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }  
+  update() {
+    if ((this.x + this.size) >= width || (this.x - this.size) <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if ((this.y + this.size) >= height || (this.y - this.size) <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+
+  collisionDetect() {
+    for (const ball of balls) {
+      if (this !== ball) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
