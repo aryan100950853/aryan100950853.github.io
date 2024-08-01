41dbe496d06637d5e991688eec6d327c1b3bb5fb
aryan100950853 <aryankumar.panchal@dcmail.ca>
2024-08-01 16:10:01 -0400

    2 commit
    Added the comments .

--- Assignment 4/p_4/main.js
+++ Assignment 4/p_4/main.js
@@ -1,3 +1,21 @@
+/*
+Name : AryanKumar Panchal
+
+Date : 2024/08/01
+
+File: main.js
+
+This JavaScript file is involved in the animation of bouncing balls 
+through creating the canvas and the drawing context. It contains 
+procedures to produce random numbers and colors, and declares a 
+`Ball` class appropriate to control for an individual ball. 
+This pseudo code used the script to generate numerous balls 
+and assigns random properties to the ball it then animates 
+the ball for a certain amount of time. The `draw()`, `update()`, 
+and `collisionDetect()` functions have the responsibility of 
+displaying, moving, and changing color upon collisions. It is 
+managed with the help of the `requestAnimationFrame()` function.
+*/
 // setup canvas
 
 const canvas = document.querySelector("canvas");
