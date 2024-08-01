0cbf8395a4f76c79ed9824b343d9e1fe5bde957c
aryan100950853 <aryankumar.panchal@dcmail.ca>
2024-08-01 13:37:13 -0400

    second commit
    Add Header comments in all files with the data of name, file type, Date and description.

--- Assignment 4/p_3/main.js
+++ Assignment 4/p_3/main.js
@@ -1,3 +1,18 @@
+/**
+ * Name: AryanKumar Panchal
+
+   File: main.js
+
+   Date: 2024/08/01
+  
+  The JavaScript file is devoted to the animation of the icons 
+  as bouncing balls, which is initiating the canvas, defining 
+  the context, creating and utilizing the Ball class, and creating 
+  25 animated balls. Regarding the management of ball positions and
+   the need to control for collisions, it employs the loop() which 
+   provides a smooth animation using the requestAnimationFrame() 
+   while changing colors every time they collide.
+ */
 // setup canvas
 
 const canvas = document.querySelector("canvas");
