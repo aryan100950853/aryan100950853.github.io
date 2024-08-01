d1362213cc522fb5d1b69d871a139e161c48d39b
aryan100950853 <aryankumar.panchal@dcmail.ca>
2024-08-01 15:40:58 -0400

    final commit
    Added initialization of `balls` array with 25 random ball objects. Implemented `loop()` function to handle animation, including drawing, updating, and collision detection for each ball. Started the animation loop with `requestAnimationFrame()`.

--- Assignment 4/p_3/main.js
+++ Assignment 4/p_3/main.js
@@ -76,3 +76,33 @@ class Ball {
     }
   }
 }
+const balls = [];
+
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    random(size, width - size),
+    random(size, height - size),
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+function loop() {
+  ctx.fillStyle = "rgb(0, 0, 0, 0.25)";
+  ctx.fillRect(0, 0, width, height);
+
+  for (const ball of balls) {
+    ball.draw();
+    ball.update();
+    ball.collisionDetect();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
